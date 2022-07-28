148d9eee3ca865dcfe02f9bbc4c29a392c43168d
Sergey-Misyura <101212758+Sergey-Misyura@users.noreply.github.com>
2022-07-28 09:47:16 +0300

    change_config

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 BOT_TOKEN=os.getenv('BOT_TOKEN')
-BOT_OWNER=os.getenv('BOT_OWNER')
-CHANNEL_ID=os.getenv('CHANNEL_ID')
+BOT_OWNER=int(os.getenv('BOT_OWNER'))
+CHANNEL_ID=int(os.getenv('CHANNEL_ID'))
 
 #private
 IS_POSTING_REQUESTED = False
